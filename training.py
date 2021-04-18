820c9b303b00f3a4f4d41b56c095f802b3e10bed
Wael <thebigguyis@gmail.com>
2021-04-19 02:36:21 +0300

    lemmatized words

--- training.py
+++ training.py
@@ -11,7 +11,8 @@ from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
 nltk.download('punkt')
-lemmatizer = WordNetLemmatizer
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -23,9 +24,12 @@ ignore_letters = ['?', '!', '.', ',']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
         
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+print(words)
